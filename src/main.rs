2cc096d7a1ab88e90d7b89e56b6b82686a81f938
d4nicoder <d4nicoder@gmail.com>
2021-12-12 10:35:15 +0100

    chore: better results logging

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ mod day2;
 mod utils;
 
 fn main() {
-    println!("Running day 1 problem 1");
+    print!("Running day 1 problem 1: ");
     let result_day1_1 = day1::run1();
-    println!("Result: {}", result_day1_1);
+    println!("{}", result_day1_1);
 
-    println!("Running day 1 problem 2");
+    print!("Running day 1 problem 2: ");
     let result_day1_2 = day1::run2();
-    println!("Result: {}", result_day1_2);
+    println!("{}", result_day1_2);
 
-    println!("Running day 2 problem 1");
+    print!("Running day 2 problem 1: ");
     let result_day2_1 = day2::run1();
-    println!("Result: {}", result_day2_1);
+    println!("{}", result_day2_1);
 
-    println!("Running day 2 problem 2");
+    print!("Running day 2 problem 2: ");
     let result_day2_2 = day2::run2();
-    println!("Result: {}", result_day2_2);
+    println!("{}", result_day2_2);
 }
